c7e3c48d7facee8c3ae36a51ac457b4d1e0d4c47
GWigWam <welwisam@gmail.com>
2025-04-11 21:31:00 +0200

    Minor improvements to RandomPlayerController

--- src/GWigWam.Machiavelli.Core/RandomPlayerController.cs
+++ src/GWigWam.Machiavelli.Core/RandomPlayerController.cs
@@ -21,13 +21,20 @@ public class RandomPlayerController(Game game, Player player) : PlayerController
         {
             actions.GetCards();
         }
+        BuildRandom(actions);
+    }
 
-        if (player.Hand.Any())
+    private void BuildRandom(BasePlayerActions actions, int count = 1)
+    {
+        for (int c = 0; c < count; c++)
         {
-            var build = player.Hand.RandomItem();
-            if (build.Card.Cost <= player.Gold)
+            if (player.Hand.Any())
             {
-                actions.Build(build);
+                var build = player.Hand.RandomItem();
+                if (build.Card.Cost <= player.Gold)
+                {
+                    actions.Build(build);
+                }
             }
         }
     }
@@ -41,7 +48,7 @@ public class RandomPlayerController(Game game, Player player) : PlayerController
     public override void PlayAssassin(Round round, AssassinActions actions)
     {
         var kill = game.Characters
-            .Where(c => c.Type != CharacterType.Known.Assassin)
+            .Where(c => c.Type != CharacterType.Known.Assassin && !round.OpenCharacters!.Contains(c))
             .RandomItem();
         actions.Assassinate(kill);
         PlayGeneric(actions);
@@ -50,7 +57,7 @@ public class RandomPlayerController(Game game, Player player) : PlayerController
     public override void PlayThief(Round round, ThiefActions actions)
     {
         var rob = game.Characters
-            .Where(c => c.Type != CharacterType.Known.Assassin && c.Type != CharacterType.Known.Thief && c != round.Assassinated)
+            .Where(c => c.Type != CharacterType.Known.Assassin && c.Type != CharacterType.Known.Thief && !round.OpenCharacters!.Contains(c) && c != round.Assassinated)
             .RandomItem();
         actions.Steal(rob);
         PlayGeneric(actions);
@@ -60,20 +67,22 @@ public class RandomPlayerController(Game game, Player player) : PlayerController
     {
         void swapWithDeck()
         {
-            var cnt = Random.Shared.Next(0, player.Hand.Count + 1);
+            var cnt = Random.Shared.Next(1, player.Hand.Count + 1);
             var toSwap = player.Hand.OrderBy(_ => Random.Shared.Next()).Take(cnt);
             actions.SwapCardsWithDeck(toSwap);
         }
 
         void swapWithPlayer()
         {
-            var target = game.Players.Where(p => p != player).OrderBy(_ => Random.Shared.Next()).First();
-            actions.SwapHandWithPlayer(target);
+            if (game.Players.Where(p => p != player && p.Hand.Count > 0).OrderBy(_ => Random.Shared.Next()).FirstOrDefault() is Player target)
+            {
+                actions.SwapHandWithPlayer(target);
+            }
         }
 
         Action act = Random.Shared.NextDouble() switch
         {
-            < 0.33 => swapWithDeck,
+            < 0.33 when player.Hand.Count > 0 => swapWithDeck,
             < 0.66 => swapWithPlayer,
             _ => () => { }
         };
@@ -81,15 +90,9 @@ public class RandomPlayerController(Game game, Player player) : PlayerController
         PlayGeneric(actions);
     }
 
-    public override void PlayKing(Round round, KingActions actions)
-    {
-        PlayGeneric(actions);
-    }
+    public override void PlayKing(Round round, KingActions actions) => PlayGeneric(actions);
 
-    public override void PlayPreacher(Round round, PreacherActions actions)
-    {
-        PlayGeneric(actions);
-    }
+    public override void PlayPreacher(Round round, PreacherActions actions) => PlayGeneric(actions);
 
     public override void PlayMerchant(Round round, MerchantActions actions)
     {
@@ -101,6 +104,7 @@ public class RandomPlayerController(Game game, Player player) : PlayerController
     {
         actions.GetTwoBuildingCards();
         PlayGeneric(actions);
+        BuildRandom(actions, 2);
     }
 
     public override void PlayCondottiero(Round round, CondottieroActions actions)
