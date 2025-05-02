98e4d71093ce34b89d38efeaa4df1fe7eccf2d88
GWigWam <welwisam@gmail.com>
2025-05-02 18:05:11 +0200

    Analysis: buildings stats (build rate vs win rate)

--- src/GWigWam.Machiavelli.Console/Analysis.cs
+++ src/GWigWam.Machiavelli.Console/Analysis.cs
@@ -20,7 +20,7 @@ public static class Analysis
                     game.Setup();
                     game.Controllers = game.Players.Select(p => (p, c: new AiPlayerController(game, p))).ToDictionary(t => t.p, t => (PlayerController)t.c);
 
-                    stats[gix] = RecordGame(game);
+                    stats[gix] = RecordGame(game, localRes);
                     task.Value++;
                 });
                 task.Value = nog;
@@ -43,7 +43,7 @@ public static class Analysis
         Func<GameStats, Player, double> kingFrac = (gs, p) => gs.PlayerKingCount[p] / (double)gs.Game.Rounds.Count;
         Func<GameStats, Player, int> fstKing = (gs, p) => gs.Game.Rounds.First().RoundKing == p ? 1 : 0;
 
-        string fPcnt(double v, double max = 1, bool inv = false) => $"[{(Math.Abs((inv ? 1 : 0) - (v / max)) is var f ? f > 0.8 ? "red" : f > 0.6 ? "darkorange" : f > 0.3 ? "yellow" : "green" : "")}]{v:P2}[/]";
+        string fPcnt(double v, double max = 1, bool inv = false) => $"[{(Math.Abs((inv ? 1 : 0) - (v / max)) is var f ? f > 0.8 ? "red" : f > 0.6 ? "darkorange" : f > 0.4 ? "yellow" : f > 0.2 ? "green" : "lime" : "")}]{v:P2}[/]";
 
         var tableKing = new Table();
         tableKing.AddColumns("Position", "King rounds", "Game start player");
@@ -79,18 +79,36 @@ public static class Analysis
             tableVictims.AddRow(dsc, at, ah, tt, th);
         }
         AnsiConsole.Write(tableVictims);
+
+        var builtStat = stats.SelectMany(gs => gs.BuiltCards)
+            .GroupBy(t => t.card)
+            .Select(g => (g.Key, bFrac: g.Sum(t => t.builtFrac) / g.Count(), wFrac: g.Sum(t => t.winnerBuilt) / g.Count(), lFrac: g.Sum(t => t.loserBuilt) / g.Count()))
+            .Select(t => (card: t.Key, t.bFrac, t.wFrac, wDiff: t.wFrac / t.bFrac, t.lFrac, lDiff: t.lFrac / t.bFrac))
+            .OrderByDescending(t => t.wDiff)
+            .ToArray();
+
+        var tableBuilt = new Table();
+        tableBuilt.AddColumns("Building", "Cnt", "Was built", "Winner built", "Diff", "Loser built", "Diff");
+        foreach (var tpl in builtStat)
+        {
+            tableBuilt.AddRow(tpl.card.ToMarkup(), $"{tpl.card.Count}", fPcnt(tpl.bFrac, 0.5, true), fPcnt(tpl.wFrac, 0.15, true), fPcnt(tpl.wDiff, 0.35, true), fPcnt(tpl.lFrac, 0.15), fPcnt(tpl.lDiff, 0.35));
+        }
+        AnsiConsole.Write(tableBuilt);
     }
 
-    private static GameStats RecordGame(Game game)
+    private static GameStats RecordGame(Game game, Resources localRes)
     {
         Player[] standings = null!;
         game.GameOver += s => standings = s;
 
         var aVict = CharacterType.Known.All.ToDictionary(c => c, _ => (target: 0, hit: 0));
         var tVict = CharacterType.Known.All.ToDictionary(c => c, _ => (target: 0, hit: 0));
+        var builtl = new List<(Player player, BuildingCard building)>();
         game.OnNewRound += r => {
             r.OnAssassinateAction += (_, t) => aVict[t.Type] = (aVict[t.Type].target + 1, aVict[t.Type].hit + (r.CharacterPlayerMap.ContainsKey(t) ? 1 : 0));
             r.OnRobAction += (_, t) => tVict[t.Type] = (tVict[t.Type].target + 1, tVict[t.Type].hit + (r.CharacterPlayerMap.ContainsKey(t) ? 1 : 0));
+
+            r.OnBuild += (p, b) => builtl.Add((p, b));
         };
 
         while (!game.NextRound()) { }
@@ -111,7 +129,16 @@ public static class Analysis
         var aFrac = aVict.ToDictionary(kvp => kvp.Key, kvp => (aCnt == 0 ? 0 : kvp.Value.target / (double)aCnt, aCnt == 0 ? 0 : kvp.Value.hit / (double)aCnt));
         var tFrac = tVict.ToDictionary(kvp => kvp.Key, kvp => (tCnt == 0 ? 0 : kvp.Value.target / (double)tCnt, tCnt == 0 ? 0 : kvp.Value.hit / (double)tCnt));
 
-        return new(game, standings, playerPickCount, playerKingCount, aFrac, tFrac);
+        var built = localRes.Deck.AllCards.Select(c => c.Card).DistinctBy(c => c.Id)
+            .Select(c =>
+            {
+                var bc = builtl.Where(t => t.building == c).Count() / (double)c.Count;
+                var win = builtl.Where(t => t.building == c && t.player == standings.First()).Count() / (double)c.Count;
+                var los = builtl.Where(t => t.building == c && t.player == standings.Last()).Count() / (double)c.Count;
+                return (c, bc, win, los);
+            }).ToArray();
+
+        return new(game, standings, playerPickCount, playerKingCount, aFrac, tFrac, built);
     }
 
     public record GameStats(
@@ -120,6 +147,7 @@ public static class Analysis
         Dictionary<(Player, CharacterType), int> PlayerPickCount,
         Dictionary<Player, int> PlayerKingCount,
         Dictionary<CharacterType, (double target, double hit)> AssassinTargets,
-        Dictionary<CharacterType, (double target, double hit)> ThiefTargets
+        Dictionary<CharacterType, (double target, double hit)> ThiefTargets,
+        (BuildingCard card, double builtFrac, double winnerBuilt, double loserBuilt)[] BuiltCards
     );
 }
